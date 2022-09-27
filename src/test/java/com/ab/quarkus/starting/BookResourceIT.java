523094b16ac3f6f0041d41e78e4f0e945c83518e
arbhard2 <arbhard2@cisco.com>
2022-09-27 12:25:58 +0530

    init quarkus project

---
+++ src/test/java/com/ab/quarkus/starting/BookResourceIT.java
@@ -0,0 +1,8 @@
+package com.ab.quarkus.starting;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class BookResourceIT extends BookResourceTest {
+    // Execute the same tests but in packaged mode.
+}
